d3c1271a97da1131da8f8eaa4b73ec29ba7241d3
migant <migant@gmail.com>
2023-10-02 15:30:25 +0100

    Tech Evaluation
    Evaluating Techniques to produce a jSON based REST API (front and backend)

---
+++ src/main/java/com/specialisterne/carpool/protocol/ResponseAlive.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.specialisterne.carpool.protocol;
+
+/**
+ *
+ * @author q
+ */
+public class ResponseAlive {
+    
+    private Integer status;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+    
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%-- 
+    Document   : index.jsp
+    Created on : 2 Oct 2023, 11:18:43
+    Author     : q
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<jsp:include page="includes/header.html" />
+
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
